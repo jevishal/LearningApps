673f129f5f2405b7f82a7f73483354d16e96b7c5
Vishal <er.vishal.kmr@gmail.com>
2022-09-25 02:14:07 +0530

    firstCommit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Method3();
+            Console.ReadLine();
+        }
+
+      static  async void Method1()
+        {
+            await Task.Run(() => {
+                Method3();
+            }) 
+
+            // Task.Run(() =>
+            // {
+            //     for (int i = 0; i < 10; i++)
+            //     {
+            //         Console.WriteLine(" Method 1");
+            //         Method2();
+                    
+            //     }
+            // });
+        }
+
+      static  async Task Method2()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(" Method 2");
+                    //Method3();
+                }
+            });
+           // return 2;
+        }
+
+      static  void Method3()
+        {
+            Console.WriteLine("I am method 3");
+            Method1();
+        }
+
+    }
+}
